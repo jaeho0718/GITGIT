2523d82d72a097bcbfbdb24b9917ed75e4a94275
LeeProgrammer <jaeho010718@gmail.com>
2021-07-11 14:37:08 +0900

    MainContentView

---
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by Lee Jaeho on 2021/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SideBar()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
