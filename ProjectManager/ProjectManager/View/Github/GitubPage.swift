c5002fffcaa47ce9f83475e78582ef03c8c5fd4d
LeeProgrammer <jaeho010718@gmail.com>
2021-07-12 17:12:59 +0900

    UI변경 및 Issue의 Comment 보기 및 추가하기 기능 추가

--- ProjectManager/ProjectManager/View/Github/GitubPage.swift
+++ ProjectManager/ProjectManager/View/Github/GitubPage.swift
@@ -20,19 +20,21 @@ struct GitubPage: View {
                 Text(repository.site ?? "url을 불러올 수 없음.").font(.caption).opacity(0.8)
             }
             Divider()
-            VStack(alignment:.leading){
+            VStack(alignment:.leading,spacing:20){
                 Text(repository.name ?? "").font(.title).bold().padding(.bottom,2)
                 Text(repository.descriptions ?? "description이 없음").font(.subheadline).padding(.bottom,2)
                 //WebView(url: url, user: viewmodel.UserInfo)
                 Section(header:Label("Issue", systemImage: "ant.circle")){
-                    List{
-                        if add_issue{
-                            AddIssueCell(add_issue: $add_issue, repository: repository)
+                    ScrollView(showsIndicators:false){
+                        LazyVStack(alignment:.center){
+                            if add_issue{
+                                AddIssueCell(add_issue: $add_issue, issues: $issues, repository: repository)
+                            }
+                            ForEach(issues){ issue in
+                                IssueCell(issue: issue, repo: repository)
+                            }
                         }
-                        ForEach(issues){ issue in
-                            IssueCell(issue: issue)
-                        }
-                    }.removeBackground()
+                    }
                 }
                 Spacer()
             }.padding(5)
@@ -51,7 +53,9 @@ struct GitubPage: View {
     }
     func setValue(){
         viewmodel.getIssues(viewmodel.UserInfo, repo: repository, complication: { value in
-            issues = value
+            withAnimation(.spring()){
+                issues = value
+            }
         })
     }
 }
@@ -60,10 +64,17 @@ struct IssueCell : View{
     @EnvironmentObject var viewmodel : ViewModel
     @State private var comment_add : Bool = false
     @State private var comment : String = ""
+    @State private var comment_Markdown : String = ""
+    @State private var selection : Int = 0
+    @State private var comments : [Comments] = []
+    @State private var show_detail : Bool = false
     var issue : Issues
+    var repo : Repository
     var body: some View{
-        GroupBox(label:Label("\(issue.title)", systemImage: "ant.fill")){
+        GroupBox{
             VStack(alignment:.leading){
+                Label("\(issue.title) #\(issue.number)", systemImage: "ant.fill")
+                Divider()
                 HStack(alignment:.center){
                     viewmodel.getImage(issue.user.login)
                         .aspectRatio(contentMode: .fill)
@@ -76,31 +87,66 @@ struct IssueCell : View{
                         Text("\(issue.created_at)에 만들어짐").font(.caption).opacity(0.5)
                     }
                     Spacer()
-                }.padding(5).background(Color.blue.opacity(0.5))
-                
-                Markdown("\(issue.body)")
-                Divider()
-                if comment_add{
-                    TextEditor(text: $comment).frame(minHeight:100,maxHeight:350)
-                }
-                HStack{
-                    Spacer()
-                    Button(action:{
-                        comment = ""
-                        comment_add.toggle()
+                    Button(action:{show_detail.toggle()
+                        if !show_detail{
+                            comment_add = false
+                        }
                     }){
-                        Text(comment_add ? "Cancel" : "Add comment")
+                        Image(systemName: show_detail ? "chevron.up.square" : "chevron.down.square")
+                    }
+                }.padding(.bottom,5)
+                if show_detail{
+                    Markdown("\(issue.body)")
+                    Divider()
+                    if !(comments.isEmpty){
+                        Section(header:Label("comments", systemImage: "bubble.left.fill")){
+                            ScrollView{
+                                LazyVStack{
+                                    ForEach(comments){ value in
+                                        CommentCell(comment:value)
+                                    }
+                                }
+                            }
+                        }
                     }
                     if comment_add{
+                        TabView(selection:$selection){
+                            TextEditor(text: $comment).frame(minHeight:100,maxHeight:350).tabItem { Text("Write") }.tag(0)
+                            Markdown("\(comment_Markdown)").frame(minHeight:100,maxHeight:350).tabItem { Text("Preview") }.tag(1)
+                        }.onChange(of: selection, perform: { value in
+                            comment_Markdown = comment
+                        })
+                        Text("MarkDown문법을 지원합니다.").font(.caption).opacity(0.7)
+                    }
+                    HStack{
+                        Spacer()
                         Button(action:{
                             comment = ""
                             comment_add.toggle()
                         }){
-                            Text("Comment")
-                        }.opacity(comment.isEmpty ? 0.5 : 1.0)
+                            Text(comment_add ? "Cancel" : "Add comment")
+                        }
+                        if comment_add{
+                            Button(action:{
+                                if !(comment.isEmpty){
+                                    viewmodel.createComments(repo: repo, number: issue.number, body: comment)
+                                    viewmodel.getComments(repo: repo, number: issue.number, complication: { value in
+                                        comments = value
+                                    })
+                                }
+                                comment = ""
+                                comment_add.toggle()
+                            }){
+                                Text("Comment")
+                            }//.opacity(comment.isEmpty ? 0.5 : 1.0)
+                        }
                     }
                 }
-            }.frame(maxWidth:.infinity)
+            }.frame(maxWidth:.infinity).onDrag({ return NSItemProvider(object: NSURL(string: issue.html_url)!)})
+        }.onAppear{
+            viewmodel.getComments(repo: repo, number: issue.number, complication: { value in
+                comments = value
+            })
         }
     }
 }
@@ -112,6 +158,7 @@ struct AddIssueCell : View{
     @State private var body_str : String = ""
     @State private var markdown_str : String = ""
     @State private var selection : Int = 0
+    @Binding var issues : [Issues]
     var repository : Repository
     var body: some View{
         GroupBox(label:Label("이슈 등록하기", systemImage: "ant.fill")){
@@ -137,15 +184,37 @@ struct AddIssueCell : View{
                     Button(action:{
                         if !(title.isEmpty){
                             viewmodel.createIssues(repo: repository, title: title, body: body_str)
+                            viewmodel.getIssues(viewmodel.UserInfo, repo: repository, complication: { value in
+                                issues = value
+                            })
                             title = ""
                             body_str = ""
                             add_issue.toggle()
                         }
                     }){
                         Text("Submit new issue")
-                    }.opacity(title.isEmpty ? 0.5 : 1.0)
+                    }//.opacity(title.isEmpty ? 0.5 : 1.0)
                 }
             }.frame(maxWidth:.infinity)
+        }
+    }
+}
+
+struct CommentCell : View{
+    @EnvironmentObject var viewmodel : ViewModel
+    var comment : Comments
+    var body: some View{
+        GroupBox(label:Label(
+            title: { HStack{
+                Text(comment.user.login)
+                Text(comment.created_at).font(.caption).opacity(0.5)
+            }},
+            icon: { viewmodel.getImage(comment.user.login)
+                .aspectRatio(contentMode: .fill)
+                .frame(width:20,height:20).clipShape(Circle())
+                .overlay(Circle().stroke(lineWidth: 1)) }
+        )){
+            Markdown("\(comment.body)")
         }
     }
 }
