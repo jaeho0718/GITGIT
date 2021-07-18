3dc64dfbde0446890bea57b8d9954f3b6d08955f
LeeProgrammer <jaeho010718@gmail.com>
2021-07-19 00:03:39 +0900

    마크다운 편집기 추가

---
+++ ProjectManager/ProjectManager/View/Memo/MarkDownEditor.swift
@@ -0,0 +1,20 @@
+//
+//  MarkDownEditor.swift
+//  ProjectManager
+//
+//  Created by Lee Jaeho on 2021/07/18.
+//
+
+import SwiftUI
+
+struct MarkDownEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MarkDownEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        MarkDownEditor()
+    }
+}
