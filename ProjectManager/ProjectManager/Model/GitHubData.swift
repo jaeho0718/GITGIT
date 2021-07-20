9b97f0e3b4816767d2a8f609582b5e71e1d5064b
LeeProgrammer <jaeho010718@gmail.com>
2021-07-20 17:27:47 +0900

    File 종류 확인 함수 추가

--- ProjectManager/ProjectManager/Model/GitHubData.swift
+++ ProjectManager/ProjectManager/Model/GitHubData.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 import Security
-
+import CodeMirror_SwiftUI
 /// To call Github Api, need user GithubAccessToken
 struct User : Codable{
     var user_name : String
@@ -24,101 +24,6 @@ struct User_Info : Codable{
     var public_repos : Int
 }
 
-/*
- {
-   "login" : "jaeho0718",
-   "public_gists" : 0,
-   "twitter_username" : null,
-   "created_at" : "2017-11-17T15:55:41Z",
-   "id" : 33758013,
-   "company" : null,
-   "blog" : "jaeho0718.github.io",
-   "collaborators" : 0,
-   "html_url" : "https:\/\/github.com\/jaeho0718",
-   "node_id" : "MDQ6VXNlcjMzNzU4MDEz",
-   "url" : "https:\/\/api.github.com\/users\/jaeho0718",
-   "organizations_url" : "https:\/\/api.github.com\/users\/jaeho0718\/orgs",
-   "repos_url" : "https:\/\/api.github.com\/users\/jaeho0718\/repos",
-   "location" : null,
-   "hireable" : null,
-   "type" : "User",
-   "following_url" : "https:\/\/api.github.com\/users\/jaeho0718\/following{\/other_user}",
-   "avatar_url" : "https:\/\/avatars.githubusercontent.com\/u\/33758013?v=4",
-   "following" : 4,
-   "total_private_repos" : 6,
-   "starred_url" : "https:\/\/api.github.com\/users\/jaeho0718\/starred{\/owner}{\/repo}",
-   "received_events_url" : "https:\/\/api.github.com\/users\/jaeho0718\/received_events",
-   "followers_url" : "https:\/\/api.github.com\/users\/jaeho0718\/followers",
-   "email" : null,
-   "owned_private_repos" : 6,
-   "two_factor_authentication" : false,
-   "name" : "LeeProgrammer",
-   "subscriptions_url" : "https:\/\/api.github.com\/users\/jaeho0718\/subscriptions",
-   "gists_url" : "https:\/\/api.github.com\/users\/jaeho0718\/gists{\/gist_id}",
-   "private_gists" : 0,
-   "plan" : {
-     "private_repos" : 9999,
-     "collaborators" : 0,
-     "space" : 976562499,
-     "name" : "pro"
-   },
-   "gravatar_id" : "",
-   "bio" : "WWDC2021 Swift Student Challenge Winner",
-   "followers" : 5,
-   "disk_usage" : 180101,
-   "updated_at" : "2021-07-09T10:40:57Z",
-   "public_repos" : 8,
-   "site_admin" : false,
-   "events_url" : "https:\/\/api.github.com\/users\/jaeho0718\/events{\/privacy}"
- }
- The data couldn’t be read because it is missing.
- {
-   "login" : "jaeho0718",
-   "public_gists" : 0,
-   "twitter_username" : null,
-   "created_at" : "2017-11-17T15:55:41Z",
-   "id" : 33758013,
-   "company" : null,
-   "blog" : "jaeho0718.github.io",
-   "collaborators" : 0,
-   "html_url" : "https:\/\/github.com\/jaeho0718",
-   "node_id" : "MDQ6VXNlcjMzNzU4MDEz",
-   "url" : "https:\/\/api.github.com\/users\/jaeho0718",
-   "organizations_url" : "https:\/\/api.github.com\/users\/jaeho0718\/orgs",
-   "repos_url" : "https:\/\/api.github.com\/users\/jaeho0718\/repos",
-   "location" : null,
-   "hireable" : null,
-   "type" : "User",
-   "following_url" : "https:\/\/api.github.com\/users\/jaeho0718\/following{\/other_user}",
-   "avatar_url" : "https:\/\/avatars.githubusercontent.com\/u\/33758013?v=4",
-   "following" : 4,
-   "total_private_repos" : 6,
-   "starred_url" : "https:\/\/api.github.com\/users\/jaeho0718\/starred{\/owner}{\/repo}",
-   "received_events_url" : "https:\/\/api.github.com\/users\/jaeho0718\/received_events",
-   "followers_url" : "https:\/\/api.github.com\/users\/jaeho0718\/followers",
-   "email" : null,
-   "owned_private_repos" : 6,
-   "two_factor_authentication" : false,
-   "name" : "LeeProgrammer",
-   "subscriptions_url" : "https:\/\/api.github.com\/users\/jaeho0718\/subscriptions",
-   "gists_url" : "https:\/\/api.github.com\/users\/jaeho0718\/gists{\/gist_id}",
-   "private_gists" : 0,
-   "plan" : {
-     "private_repos" : 9999,
-     "collaborators" : 0,
-     "space" : 976562499,
-     "name" : "pro"
-   },
-   "gravatar_id" : "",
-   "bio" : "WWDC2021 Swift Student Challenge Winner",
-   "followers" : 5,
-   "disk_usage" : 180101,
-   "updated_at" : "2021-07-09T10:40:57Z",
-   "public_repos" : 8,
-   "site_admin" : false,
-   "events_url" : "https:\/\/api.github.com\/users\/jaeho0718\/events{\/privacy}"
- }
- */
 
 /// Structure that have repositroy information
 struct Repository_Info : Codable{
@@ -129,89 +34,6 @@ struct Repository_Info : Codable{
     var description : String? //Repository description
     var language : String? //Repository language
 }
-/*
- {
-   "keys_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/keys{\/key_id}",
-   "statuses_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/statuses\/{sha}",
-   "issues_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/issues{\/number}",
-   "license" : null,
-   "issue_events_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/issues\/events{\/number}",
-   "has_projects" : true,
-   "id" : 274156038,
-   "default_branch" : "master",
-   
-   "events_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/events",
-   "subscription_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/subscription",
-   "watchers" : 1,
-   "git_commits_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/git\/commits{\/sha}",
-   "subscribers_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/subscribers",
-   "clone_url" : "https:\/\/github.com\/wwdc-kr\/Assets.git",
-   "has_wiki" : true,
-   "url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets",
-   "pulls_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/pulls{\/number}",
-   "fork" : false,
-   "notifications_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/notifications{?since,all,participating}",
-   "description" : "WWDC Scholars Korea 에서 사용하는 에셋 모음입니다. ",
-   "collaborators_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/collaborators{\/collaborator}",
-   "deployments_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/deployments",
-   "archived" : false,
-   "languages_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/languages",
-   "has_issues" : true,
-   "comments_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/comments{\/number}",
-   "private" : true,
-   "size" : 1348,
-   "git_tags_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/git\/tags{\/sha}",
-   "updated_at" : "2020-07-12T12:25:24Z",
-   "ssh_url" : "git@github.com:wwdc-kr\/Assets.git",
-   "name" : "Assets",
-   "contents_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/contents\/{+path}",
-   "archive_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/{archive_format}{\/ref}",
-   "milestones_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/milestones{\/number}",
-   "blobs_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/git\/blobs{\/sha}",
-   "node_id" : "MDEwOlJlcG9zaXRvcnkyNzQxNTYwMzg=",
-   "contributors_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/contributors",
-   "open_issues_count" : 0,
-   "permissions" : {
-     "admin" : false,
-     "push" : true,
-     "pull" : true
-   },
-   "forks_count" : 0,
-   "trees_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/git\/trees{\/sha}",
-   "svn_url" : "https:\/\/github.com\/wwdc-kr\/Assets",
-   "commits_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/commits{\/sha}",
-   "created_at" : "2020-06-22T14:13:22Z",
-   "forks_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/forks",
-   "has_downloads" : true,
-   "mirror_url" : null,
-   "homepage" : "",
-   "teams_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/teams",
-   "branches_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/branches{\/branch}",
-   "disabled" : false,
-   "issue_comment_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/issues\/comments{\/number}",
-   "merges_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/merges",
-   "git_refs_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/git\/refs{\/sha}",
-   "git_url" : "git:\/\/github.com\/wwdc-kr\/Assets.git",
-   "forks" : 0,
-   "open_issues" : 0,
-   "hooks_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/hooks",
-   "html_url" : "https:\/\/github.com\/wwdc-kr\/Assets",
-   "stargazers_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/stargazers",
-   "assignees_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/assignees{\/user}",
-   "compare_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/compare\/{base}...{head}",
-   "full_name" : "wwdc-kr\/Assets",
-   "tags_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/tags",
-   "releases_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/releases{\/id}",
-   "pushed_at" : "2020-06-24T09:22:33Z",
-   "labels_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/labels{\/name}",
-   "downloads_url" : "https:\/\/api.github.com\/repos\/wwdc-kr\/Assets\/downloads",
-   "stargazers_count" : 1,
-   "watchers_count" : 1,
-   "language" : null,
-   "has_pages" : false
- },
- 
- */
 
 struct Repositories_Info : Codable{
     var total_count : Int
@@ -274,7 +96,7 @@ struct GitFile : Codable,Hashable{
     func getIcon()->Image{
         switch type{
         case "file":
-            return Image("coding")
+            return FileType.getType(self.name).getImage()
         case "dir":
             return Image("folder")
         default:
@@ -286,4 +108,121 @@ struct GitFile : Codable,Hashable{
 struct GitLinks : Codable,Hashable{
     var git : String?
     var html : String?
+}
+
+struct FileType{
+    enum type{
+        case swift,html,md,c,cpp,java,php,cs,txt,kt,python,none
+        var name : String{
+            switch self{
+            case .swift:
+                return "swift"
+            case .html:
+                return "html"
+            case .md:
+                return "md"
+            case .c:
+                return "c"
+            case .cpp:
+                return "cpp"
+            case .java:
+                return "java"
+            case .php:
+                return "php"
+            case .cs:
+                return "cs"
+            case .none:
+                return "none"
+            case .txt:
+                return "txt"
+            case .kt:
+                return "kt"
+            case .python:
+                return "py"
+            }
+        }
+        var code_mode : CodeMode{
+            switch self {
+            case .swift:
+                return .swift
+            case .html:
+                return .html
+            case .md:
+                return .markdown
+            case .c:
+                return .c
+            case .cpp:
+                return .cplus
+            case .java:
+                return .java
+            case .php:
+                return .php
+            case .cs:
+                return .css
+            case .none:
+                return .markdown
+            case .txt:
+                return .text
+            case .kt:
+                return .kotlin
+            case .python:
+                return .python
+            }
+        }
+        func getImage()->Image{
+            switch self {
+            case .swift:
+                return Image(systemName: "swift")
+            case .html:
+                return Image("html")
+            case .md:
+                return Image("coding")
+            case .c:
+                return Image("coding")
+            case .cpp:
+                return Image("cplus")
+            case .java:
+                return Image("coding")
+            case .php:
+                return Image("php")
+            case .cs:
+                return Image("css")
+            case .none:
+                return Image("coding")
+            case .txt:
+                return Image("txt")
+            case .kt:
+                return Image("coding")
+            case .python:
+                return Image("python")
+            }
+        }
+    }
+    static func getType(_ filename : String)->type{
+        let arr = filename.components(separatedBy: ".")
+        if let type_name = arr.last{
+            switch type_name {
+            case type.swift.name:
+                return type.swift
+            case type.html.name:
+                return type.html
+            case type.md.name:
+                return type.md
+            case type.c.name:
+                return type.c
+            case type.cpp.name:
+                return type.cpp
+            case type.java.name:
+                return type.java
+            case type.php.name:
+                return type.php
+            case type.cs.name:
+                return type.cs
+            default:
+                return type.none
+            }
+        }else{
+            return type.none
+        }
+    }
 }
