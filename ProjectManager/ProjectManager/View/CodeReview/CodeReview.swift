249e2971cbebdf23a01424e193e527a63d0bd053
LeeProgrammer <jaeho010718@gmail.com>
2021-07-22 00:20:08 +0900

    코드리뷰 창 추가

--- ProjectManager/ProjectManager/View/CodeReview/CodeReview.swift
+++ ProjectManager/ProjectManager/View/CodeReview/CodeReview.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CodeMirror_SwiftUI
+import MarkdownUI
 
 struct CodeReview: View {
     var data : Code
@@ -15,8 +16,8 @@ struct CodeReview: View {
         GeometryReader{ geomtry in
             let width = geomtry.size.width/2
             HSplitView{
-                CodeReviewCode(code: $code).frame(minWidth:width,maxWidth:.infinity)
-                CodeReviews().frame(minWidth:width,maxWidth:.infinity)
+                CodeReviewCode(code: $code,data:data).frame(minWidth:width,maxWidth:.infinity)
+                CodeReviews(data:data).frame(minWidth:width,maxWidth:.infinity)
             }
         }.onAppear{
             code = data.code ?? ""
@@ -26,10 +27,13 @@ struct CodeReview: View {
 
 struct CodeReviewCode : View{
     @SceneStorage("fontSize") var fontSize : Int = 12
+    @Environment(\.colorScheme) var colorScheme
+    @EnvironmentObject var viewmodel : ViewModel
     @Binding var code : String
+    var data : Code
     var body: some View{
         ZStack(alignment:.bottom){
-            CodeView(theme: .irBlack, code: $code, mode: CodeMode.python.mode(), fontSize: fontSize, showInvisibleCharacters: true, lineWrapping: true)
+            CodeView(theme: colorScheme == .dark ? SettingValue.getTheme(viewmodel.settingValue.code_type_dark) : SettingValue.getTheme(viewmodel.settingValue.code_type_light), code: $code, mode: FileType.getType(data.title ?? "").code_mode.mode(), fontSize: fontSize, showInvisibleCharacters: true, lineWrapping: true)
             HStack{
                 Text("words : \(code.count)").font(.caption2).padding(.leading,5)
                 Spacer()
@@ -50,28 +54,97 @@ struct CodeReviewCode : View{
 }
 
 struct CodeReviews : View{
+    var data : Code
+    @EnvironmentObject var viewmodel : ViewModel
+    @State private var addReview : Bool = false
     var body: some View{
         List{
-            AddReviewCell()
+            if addReview{
+                AddReviewCell(data:data,add:$addReview)
+            }
+            ForEach(viewmodel.CodeReviews.filter{$0.reviewID == data.reviewID}){ review in
+                ReviewCell(data: review,type: data.title ?? "")
+            }.onDelete(perform: deleteReview)
+        }.toolbar{
+            ToolbarItem{
+                Button(action:{addReview.toggle()}){
+                    Text(addReview ? "Cancel" : "Add Review")
+                }
+            }
+        }
+    }
+    
+    func deleteReview(at indexOffset : IndexSet){
+        DispatchQueue.main.async {
+            indexOffset.forEach{ index in
+                let review = viewmodel.CodeReviews.filter{$0.reviewID == data.reviewID}[index]
+                viewmodel.deleteData(review)
+            }
+            viewmodel.fetchData()
         }
     }
 }
 
 struct AddReviewCell : View{
+    var data : Code
+    @Environment(\.colorScheme) var colorScheme
+    @EnvironmentObject var viewmodel : ViewModel
+    @Binding var add : Bool
     @State private var comments : String = ""
     @State private var code : String = "Hello_World"
     var body: some View{
         VStack{
-            CodeView(code: $code, mode: CodeMode.python.mode()).frame(minHeight:150).padding(.top,5)
+            CodeView(theme: colorScheme == .dark ? SettingValue.getTheme(viewmodel.settingValue.code_type_dark) : SettingValue.getTheme(viewmodel.settingValue.code_type_light), code: $code, mode: CodeMode.swift.mode(), showInvisibleCharacters: false, lineWrapping: false).frame(minHeight:150).padding(.top,5)
             MarkDownEditor(memo: $comments)
+            HStack{
+                Spacer()
+                Button(action:{
+                    add.toggle()
+                }){
+                    Text("Cancel")
+                }.buttonStyle(AddButtonStyle())
+                Button(action:{
+                    viewmodel.saveCodeComment(reviewID: data.reviewID, code: code, review: comments)
+                    DispatchQueue.main.async {
+                        viewmodel.fetchData()
+                    }
+                    add.toggle()
+                }){
+                    Text("Add")
+                }.buttonStyle(AddButtonStyle())
+            }
         }.padding(5).background(VisualEffectView(material: .underPageBackground, blendingMode: .withinWindow)).cornerRadius(5)
     }
 }
 
 struct ReviewCell : View{
+    @Environment(\.colorScheme) var colorScheme
+    @EnvironmentObject var viewmodel : ViewModel
+    @State private var showDetail : Bool = false
+    var data : CodeComment
+    var type : String
+    var keyword : String{
+        let ko_word = KoRank(data.review ?? "")
+        let result = ko_word.run()
+        return result.keyword
+    }
     var body: some View{
-        GroupBox{
-            
-        }
+        VStack{
+            HStack{
+                Text(keyword).font(.headline).help("자동으로 추출된 키워드입니다.")
+                Spacer()
+                Button(action:{
+                    withAnimation(.spring()){
+                        showDetail.toggle()
+                    }
+                }){
+                    Image(systemName: showDetail ? "arrow.down.right.and.arrow.up.left.circle.fill" : "arrow.up.left.and.arrow.down.right.circle.fill")
+                }.buttonStyle(RemoveBackgroundStyle())
+            }.frame(maxWidth:.infinity,maxHeight: 30)
+            if !showDetail{
+                CodeView(theme: colorScheme == .dark ? SettingValue.getTheme(viewmodel.settingValue.code_type_dark) : SettingValue.getTheme(viewmodel.settingValue.code_type_light), code: .constant(data.code ?? ""), mode: FileType.getType(type).code_mode.mode(), showInvisibleCharacters: false, lineWrapping: false).frame(minHeight:150).padding(.top,5).allowsHitTesting(false)
+                Markdown("\(data.review ?? "")").padding(.bottom,5)
+            }
+        }.padding(5).background(VisualEffectView(material: .underPageBackground, blendingMode: .withinWindow)).clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
