3b75467f5957ddd3316a5b11cc6ed2aa323c7d82
LeeProgrammer <jaeho010718@gmail.com>
2021-07-21 17:30:24 +0900

    설정화면 추가

---
+++ ProjectManager/ProjectManager/Model/SettingValue.swift
@@ -0,0 +1,8 @@
+//
+//  SettingValue.swift
+//  ProjectManager
+//
+//  Created by Lee Jaeho on 2021/07/21.
+//
+
+import Foundation
---
+++ ProjectManager/ProjectManager/View/Setting/Setting.swift
@@ -0,0 +1,20 @@
+//
+//  Setting.swift
+//  ProjectManager
+//
+//  Created by Lee Jaeho on 2021/07/20.
+//
+
+import SwiftUI
+
+struct Setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Setting_Previews: PreviewProvider {
+    static var previews: some View {
+        Setting()
+    }
+}
