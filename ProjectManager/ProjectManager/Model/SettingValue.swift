793c09a572e9f937647375911ae79ccf3d34f881
LeeProgrammer <jaeho010718@gmail.com>
2021-07-22 00:19:40 +0900

    Setting Data Structure

--- ProjectManager/ProjectManager/Model/SettingValue.swift
+++ ProjectManager/ProjectManager/Model/SettingValue.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+import CodeMirror_SwiftUI
+
+struct SettingValue : Codable{
+    /// Program Language type
+    enum language : String,Identifiable,CaseIterable,Codable{
+        case English,Korean
+        var id : Int{
+            hashValue
+        }
+        var name : LocalizedStringKey{
+            switch self{
+            case .English:
+                return "영어"
+            case .Korean:
+                return "한국어"
+            }
+        }
+    }
+    
+    var language_type : language
+    var onAutoKeyword : Bool
+    var recomandSearch : Bool
+    var code_type_light : String
+    var code_type_dark : String
+    
+    static func getTheme(_ type : String)->CodeViewTheme{
+        if let theme = CodeViewTheme.allCases.first(where: {$0.rawValue == type}){
+            return theme
+        }else{
+            return .irBlack
+        }
+    }
+
+}
